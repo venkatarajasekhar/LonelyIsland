bc8d0760bfabef98576d7c9dd4f7e400e6f4d708
aSealBack <makarov.alexander.e@gmail.com>
2016-04-13 23:49:37 +0300

    Some refactoring: moved largs methods from h to cpp

--- Engine.h
+++ Engine.h
@@ -19,55 +19,13 @@ class Engine : public Singleton<Engine> {
 	const TCHAR *lpszWindowClass = _T("lonelyisland");
 	const TCHAR *lpszTitle = _T("DirectX App");
 	const TCHAR *plszAppName = _T("Lonely Island");
-	HRESULT CreateUserWindow(HINSTANCE hInstance, int nCmdShow) {
-		WNDCLASSEX Window;
 
-		Window.cbSize = sizeof(WNDCLASSEX);
-		Window.style = CS_HREDRAW | CS_VREDRAW;
-		// TODO: Substitute message processing function here
-		Window.lpfnWndProc = (WNDPROC) &pMessageManager->MessageDispatcher;
-		Window.cbClsExtra = 0;
-		Window.cbWndExtra = 0;
-		Window.hInstance = hInstance;
-		Window.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
-		Window.hCursor = LoadCursor(NULL, IDC_ARROW);
-		Window.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-		Window.lpszMenuName = NULL;
-		Window.lpszClassName = lpszWindowClass;
-		Window.hIconSm = LoadIcon(Window.hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
+	HRESULT CreateUserWindow(HINSTANCE hInstance, int nCmdShow);
 
-		if (!RegisterClassEx(&Window)) {
-			MessageBox(NULL, _T("Call to RegisterClassEx failed!"),
-				plszAppName, NULL);
-			return 1;
-		}
-
-		HWND hWnd = CreateWindow(lpszWindowClass, lpszTitle,
-			WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
-			500, 100, NULL, NULL,
-			hInstance, NULL);
-		if (!hWnd) {
-			MessageBox(NULL, _T("Call to CreateWindow failed!"),
-				plszAppName, NULL);
-			return 1;
-		}
-
-		ShowWindow(hWnd, nCmdShow);
-		UpdateWindow(hWnd);
-
-
-		return 0;
-	}
 public:
 	void InitWithProgramInstance(HINSTANCE hInstance) {
 		CreateUserWindow(hInstance, SW_SHOWMAXIMIZED);
 	}
 
-	void Run() {
-		MSG Msg;
-		while (GetMessage(&Msg, NULL, 0, 0)) {
-			TranslateMessage(&Msg);
-			DispatchMessage(&Msg);
-		}
-	}
+	void Run();
 };
